9461018037942ecfef36f32ad0a46a9bf91e31b1
arif99050 <arifulislam99050@gmail.com>
2022-05-07 17:05:04 +0600

    connect with mongo

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jozew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+ console.log("con")
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/',(req,res)=>{
     res.send("connected")
 })
