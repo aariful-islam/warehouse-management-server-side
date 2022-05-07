5afa2bddc9a2d6af774c654ffe31bf1b343603f0
arif99050 <arifulislam99050@gmail.com>
2022-05-08 01:21:16 +0600

    get data from mongo

--- index.js
+++ index.js
@@ -12,14 +12,40 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jozew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jozew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+const uri = "mongodb+srv://dbuser1:U4FmJs0HFZYnml0e@cluster0.5tvog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
- console.log("con")
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        console.log("done")
+        await client.connect();
+        const productCollection = client.db('laptopWarehouse').collection('laptop');
+
+    
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 
 app.get('/',(req,res)=>{
